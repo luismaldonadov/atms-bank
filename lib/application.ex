9fbc72f1c5811b1729694bba286cca7744e4edba
Luis Maldonado <maldonadovla@gmail.com>
2020-11-08 14:55:36 -0600

    Add function docs

--- lib/application.ex
+++ lib/application.ex
@@ -1,4 +1,9 @@
 defmodule Atm.Application do
+  @moduledoc """
+    Entry application point that starts the GenSever process
+    for Bancomer, Santander and Hsbc banks.
+  """
+
   use Application
 
   @banks bancomer: Bancomer, santander: Santander, hsbc: Hsbc
